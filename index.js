5b150a67a23c4322e319b3fa7703a757dc3a6977
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-05-01 10:52:10 +0100

    Changed code to use  async/await

--- index.js
+++ index.js
@@ -18,9 +18,41 @@ const data1 = {
   "creator": "Chef Homo Erectus"
 };
 
-// Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+//Fully working Async/Await
+
+async function myRecipeApp () {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    await console.log(`Connected to the database`);
+    await Recipe.deleteMany();
+  
+    const recipe = await Recipe.create(data1);
+    await console.log(`Iteration 2: ${recipe.title}`);
+  
+    const recipes = await Recipe.insertMany(data);
+    await recipes.forEach(recipe => console.log(`Iteration 3: ${recipe.title}`));
+          
+    const updatedRecipe = await Recipe.findOneAndUpdate({title : "Rigatoni alla Genovese" }, { duration : 100 }, { new : true });
+    await console.log(`Iteration 4: ${updatedRecipe.title} cooking time has been updated to ${updatedRecipe.duration} minutes`);
+            
+    await Recipe.deleteOne({ title : "Carrot Cake" });
+    await console.log(`Iteration 5: I am sorry but Carrot Cake is no longer available.`);
+          
+    await mongoose.connection.close();
+    await console.log('Iteration 6: Mongoose default connection disconnected through app termination');
+    await process.exit(0);
+
+  } catch (error) {
+    console.error('This error has happened:', error);
+  }
+}
+
+myRecipeApp ();
+
+/*
+Fully working - Callback Hell
+
+mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
@@ -51,3 +83,44 @@ mongoose
   .catch(error => {
     console.error('This error has happened:', error);
   });
+
+_________________________________________________________________________________________________________________________
+
+NOT WORKING - Chaining 
+
+  The first console.log is executed "Connected to the database: 'recipe-app'" but this is followed by:
+  "This error has happened: MongoNotConnectedError: MongoClient must be connected to perform this operation"
+
+mongoose.connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any recipes to the database, let's remove all existing ones
+    return Recipe.deleteMany();
+  })
+  .then(() => {
+    return Recipe.create(data1);
+  })
+  .then(recipe => {
+    console.log(`Iteration 2: ${recipe.title}`);
+    return Recipe.insertMany(data);
+  })
+  .then( recipes => {
+    recipes.forEach(recipe => console.log(`Iteration 3: ${recipe.title}`));
+    return Recipe.findOneAndUpdate({title : "Rigatoni alla Genovese" }, { duration : 100 }, { new : true })
+  })        
+  .then (updatedRecipe => {
+    console.log(`Iteration 4: ${updatedRecipe.title} cooking time has been updated to ${updatedRecipe.duration} minutes`);
+    return Recipe.deleteOne({ title : "Carrot Cake" })
+  })         
+  .then(finish => {
+    console.log(`Iteration 5: I am sorry but Carrot Cake is no longer available.`);
+  })
+  .then (mongoose.connection.close(() => {   
+      console.log('Iteration 6: Mongoose default connection disconnected through app termination');
+      return process.exit(0);
+  }))
+  .catch(error => {
+    console.error('This error has happened:', error);
+  });
+
+*/
